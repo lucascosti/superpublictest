8673859757e289b2c3760d23b6016604b471e63b
Lucas Costi <lucascosti@github.com>
2019-09-10 13:41:08 +1000

    clean out old actions files

--- notify-issue-author.action/index.js
+++
@@ -1,7 +0,0 @@
-const { Toolkit } = require('actions-toolkit');
-const notifyIssueAuthor = require('./lib/notify-issue-author');
-
-Toolkit.run(notifyIssueAuthor, {
-  event: 'issues.closed',
-  secrets: ['GITHUB_TOKEN']
-});
